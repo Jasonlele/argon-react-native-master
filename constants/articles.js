46b73163088bae26d48d8b461535da2fae6339af
Jasonlele <71078854+Jasonlele@users.noreply.github.com>
2021-08-30 20:07:54 +0800

    www

--- constants/articles.js
+++ constants/articles.js
@@ -1,28 +1,28 @@
 export default [
   {
-    title: 'Online Booking',
-    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/booking.png',
+    title: 'Ice cream is made with carrageenan …',
+    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
     cta: 'View article', 
     horizontal: true
   },
   {
-    title: 'Symptom Checker',
-    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/Symptom.png',
+    title: 'Is makeup one of your daily esse …',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
     cta: 'View article'
   },
   {
-    title: 'Basic personal information',
+    title: 'Coffee is more than just a drink: It’s …',
     image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
     cta: 'View article' 
   },
   {
-    title: 'My Medicare',
-    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/Medicare.png',
+    title: 'Fashion is a popular style, especially in …',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
     cta: 'View article' 
   },
   {
-    title: 'Emergency Call',
-    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/Emergency.png',
+    title: 'Argon is a great free UI packag …',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
     cta: 'View article', 
     horizontal: true
   },
