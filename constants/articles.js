f2296c2f653b54b21ebae319e63d030af3120668
Jasonlele <71078854+Jasonlele@users.noreply.github.com>
2021-08-30 19:40:07 +0800

    change home images

--- constants/articles.js
+++ constants/articles.js
@@ -1,13 +1,13 @@
 export default [
   {
     title: 'Online Booking',
-    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/booking.png',
     cta: 'View article', 
     horizontal: true
   },
   {
     title: 'Symptom Checker',
-    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/Symptom.png',
     cta: 'View article'
   },
   {
@@ -17,12 +17,12 @@ export default [
   },
   {
     title: 'My Medicare',
-    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/Medicare.png',
     cta: 'View article' 
   },
   {
     title: 'Emergency Call',
-    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    image: 'https://github.com/Jasonlele/argon-react-native-master/blob/main/assets/imgs/Emergency.png',
     cta: 'View article', 
     horizontal: true
   },
