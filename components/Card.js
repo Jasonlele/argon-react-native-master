780ecb072e32e539b1744e8e11f4846dc28d861c
Jasonlele <71078854+Jasonlele@users.noreply.github.com>
2021-08-30 20:09:16 +0800

    sas

--- components/Card.js
+++ components/Card.js
@@ -31,7 +31,7 @@ class Card extends React.Component {
         <TouchableWithoutFeedback onPress={() => navigation.navigate('Pro')}>
           <Block flex space="between" style={styles.cardDescription}>
             <Text size={14} style={styles.cardTitle}>{item.title}</Text>
-            <Button>use</Button>
+            <Button>Try Now</Button>
             <Text size={13} muted={!ctaColor} color={ctaColor || argonTheme.COLORS.ACTIVE} bold>{item.cta}</Text>
           </Block>
         </TouchableWithoutFeedback>
