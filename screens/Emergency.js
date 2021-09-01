e3c21893f3529c2f345df8cb4c4b2e5968a9fdb1
Jiale DONG <wuleidabendan@gmail.com>
2021-09-01 14:09:29 +0800

    fix -1.4

---
+++ screens/Emergency.js
@@ -0,0 +1,9 @@
+import React from "react";
+import call from 'react-native-phone-call'
+
+const args = {
+  number: '9093900003', // String value with the number to call
+  prompt: false // Optional boolean property. Determines if the user should be prompt prior to the call 
+}
+
+call(args).catch(console.error)
