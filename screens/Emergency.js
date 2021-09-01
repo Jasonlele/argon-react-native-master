3f21e3ec35c0a7248df78e1175933397fb149373
Jiale DONG <wuleidabendan@gmail.com>
2021-09-01 14:14:56 +0800

    s

--- screens/Emergency.js
+++ screens/Emergency.js
@@ -1,9 +1,16 @@
 import React from "react";
 import call from 'react-native-phone-call'
-
+import { Button } from "../components";
 const args = {
-  number: '9093900003', // String value with the number to call
-  prompt: false // Optional boolean property. Determines if the user should be prompt prior to the call 
+    number: '9093900003', // String value with the number to call
+    prompt: false // Optional boolean property. Determines if the user should be prompt prior to the call 
+  };
+class Emergency extends React.Component {
+    render() {
+        return(
+            <Button onPress = {call(args).catch(console.error)}></Button> 
+        )
+    }
 }
 
-call(args).catch(console.error)
+export default Emergency;
