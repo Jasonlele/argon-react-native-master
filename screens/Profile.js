bb0ff5f0acbf1f54a35f2f4b1bce040b50841983
Jiale DONG <wuleidabendan@gmail.com>
2021-10-27 19:30:40 +0800

    s

--- screens/Profile.js
+++ screens/Profile.js
@@ -24,11 +24,9 @@ import * as SQLite from "expo-sqlite";
 import ModalDropdown from 'react-native-modal-dropdown';
 
 const { width, height } = Dimensions.get("screen");
-
 const thumbMeasure = (width - 48 - 32) / 3;
 
 class Profile extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -40,42 +38,38 @@ class Profile extends React.Component {
     
 
   }
-
+  //build and connect data base 
   RefreshPage=()=>{
         const{firstSearchPageData}= this.state
     
         db.transaction((tx) => {
-   
-    
-            tx.executeSql("select bookname from BookingDetail where phone = ? and finish = 'no'", 
-            [test],
-             (_, result) =>{
-              var len = result.rows.length;                      
-              let result1=[]
-                if(len>0){
-                 
-                  for(let i=0; i<len; i++){
-                    // result1.push(result.rows.item(i).date +"      "+ result.rows.item(i).name +"      "+ result.rows.item(i).hostipal)
-                    // console.log(JSON.stringify(result.rows.item(i).date))
-                    result1.push(result.rows.item(i).bookname)
-                  }
-                  this.setState({firstSearchPageData :result1})        
-                } 
-              
-             }
-              );
-        });
+          tx.executeSql("select bookname from BookingDetail where phone = ?", 
+          [test],
+            (_, result) =>{
+            var len = result.rows.length;                      
+            let result1=[]
+              if(len>0){
+                for(let i=0; i<len; i++){
+                  // result1.push(result.rows.item(i).date +"      "+ result.rows.item(i).name +"      "+ result.rows.item(i).hostipal)
+                  // console.log(JSON.stringify(result.rows.item(i).date))
+                  result1.push(result.rows.item(i).bookname)
+                }
+                this.setState({firstSearchPageData :result1})        
+              } 
+            }
+            );
+      });
   }
   
   render() {
     const { navigation } = this.props;
     const{imageUri, insurance, firstSearchPageData} = this.state;
     global.useUri = imageUri;
-    //数据库操作
+    //database operation
 
     const db = SQLite.openDatabase("db.DECO3801");
 
-    //删除表，请一定要注释
+    //删除表，请一定要注释 delete database
     // db.transaction((tx) => {
     //   tx.executeSql(
     //     "DROP TABLE BookingDetail;"
@@ -84,30 +78,22 @@ class Profile extends React.Component {
     
     // });
 
-
+    //pick the appointment data from database
     db.transaction((tx) => {
       tx.executeSql("select uri from ProfileImage where phone = ?", 
       [test],
        (_, result) =>{
           var len = result.rows.length;
           if(len>0){
-           
-            //????
             this.setState({imageUri:result.rows.item(len-1).uri})
-            
-
-            
           } 
-          
-       }
-        );
-
-      
+       }); 
     });
 
 
     return (
       
+      //profile container
       <Block flex style={styles.profile}>
         <Block flex>
           <ImageBackground
@@ -128,12 +114,8 @@ class Profile extends React.Component {
               titleStyle = {{fontWeight: "bold", fontSize:25, marginLeft:35, fontFamily: 'serif'}} />
 
               <Block flex style={styles.profileCard}>
-             
-
                 <Block style={styles.avatarContainer}>
-                  
                   <Image source={{ uri: imageUri }} style={styles.avatar} />
-
                   <Block style ={{marginLeft:40}}>
                     <TouchableWithoutFeedback onPress={() => navigation.navigate('uploadProfile')}>
                     <FontAwesome5 
@@ -167,29 +149,26 @@ class Profile extends React.Component {
                 </Block>
               </Block>
 
-
 {/* 
               从数据库里取出的值     */}
           <Block style={styles.appCard}>
-          <TouchableWithoutFeedback onPress={this.RefreshPage}>
-              <Block flex style={styles.appointment}>
-                <Block>
+            <TouchableWithoutFeedback onPress={this.RefreshPage}>
+            <Block flex style={styles.appointment}>
+              <Block>
                 <View style={ {flexDirection:'row', marginTop:1, marginLeft:20}}>
-                      <View>
-                      <TouchableWithoutFeedback onPress={() => navigation.navigate('Booking')}>
-                      <MaterialCommunityIcons style={{marginTop:2}} name="timeline-plus" size={32} color="black" />
-                       
-                      </TouchableWithoutFeedback> 
-                      </View>
-                      <View>
-                      <TouchableWithoutFeedback onPress={() => navigation.navigate('Booking')}>
-                      <Text style ={{fontSize:25, marginLeft:20, fontWeight:'bold', marginBottom:2}}>My Appointment</Text>
-                      </TouchableWithoutFeedback> 
-                      </View>
-                      
+                  <View>
+                    <TouchableWithoutFeedback onPress={() => navigation.navigate('Booking')}>
+                    <MaterialCommunityIcons style={{marginTop:2}} name="timeline-plus" size={32} color="black" />
+                    </TouchableWithoutFeedback> 
                   </View>
-                </Block>
-                <ScrollView>
+                <View>
+                    <TouchableWithoutFeedback onPress={() => navigation.navigate('Booking')}>
+                    <Text style ={{fontSize:25, marginLeft:20, fontWeight:'bold', marginBottom:2}}>My Appointment</Text>
+                    </TouchableWithoutFeedback> 
+                  </View>   
+                </View>
+              </Block>
+              <ScrollView>
                 {firstSearchPageData.map((item) => {
                       return (
                       <Block key={item}>                       
@@ -199,71 +178,58 @@ class Profile extends React.Component {
                       </Block>
                         )
                 })}
-                </ScrollView>
-              </Block>
-              </TouchableWithoutFeedback>
-              </Block>   
+              </ScrollView>
+            </Block>
+            </TouchableWithoutFeedback>
+          </Block>   
 
-              <Block style={styles.insuranceCard}>
-                <Block flex style={styles.information}>
-                <View style={ {flexDirection:'row', marginTop:15}}>
-                      <View>
-                      <TouchableWithoutFeedback onPress={() => navigation.navigate('Insurance')}>
-                      <FontAwesome5 name="hand-sparkles" size={28} color="black" />
-                      </TouchableWithoutFeedback>
-                      </View>
-                      <View>
-                      <TouchableWithoutFeedback onPress={() => navigation.navigate('Insurance')}>
-                      <Text style ={{fontSize:25, fontWeight:"bold", marginLeft:30,marginTop: 0}}>My Insurance</Text>
-                      </TouchableWithoutFeedback>
-                      </View>
-                  </View>
-                  
-                  <ModalDropdown defaultValue=" Choose Types" 
-                      dropdownStyle= {{marginLeft:0}} 
-                      dropdownTextStyle={{fontSize:25}} 
-                      textStyle={{fontSize:25, fontWeight:"bold", marginTop: 20, marginLeft: 15}} 
-                      options={["StudyingInsurance", "VisitingInsurance", "CitizenInsurance"]}
-                      onSelect = {(value) => this.setState({insurance:(String(this.state.insurance[value]))})}
-                      />
-                        
-                       
-                </Block>
+          <Block style={styles.insuranceCard}>
+            <Block flex style={styles.information}>
+              <View style={ {flexDirection:'row', marginTop:15}}>
+                <View>
+                  <TouchableWithoutFeedback onPress={() => navigation.navigate('Insurance')}>
+                  <FontAwesome5 name="hand-sparkles" size={28} color="black" />
+                  </TouchableWithoutFeedback>
+                </View>
+                <View>
+                  <TouchableWithoutFeedback onPress={() => navigation.navigate('Insurance')}>
+                  <Text style ={{fontSize:25, fontWeight:"bold", marginLeft:30,marginTop: 0}}>My Insurance</Text>
+                  </TouchableWithoutFeedback>
+                </View>
+              </View>
+              <ModalDropdown defaultValue=" Choose Types" 
+                    dropdownStyle= {{marginLeft:0}} 
+                    dropdownTextStyle={{fontSize:25}} 
+                    textStyle={{fontSize:25, fontWeight:"bold", marginTop: 20, marginLeft: 15}} 
+                    options={["StudyingInsurance", "VisitingInsurance", "CitizenInsurance"]}
+                    onSelect = {(value) => this.setState({insurance:(String(this.state.insurance[value]))})}
+                    />   
               </Block>
+          </Block>
                   
 
-              <Block style={styles.apphisCard}>
-
-              <TouchableWithoutFeedback onPress={() => navigation.navigate('History')}>
-              <View style={ {flexDirection:'row', marginTop:15, marginLeft:18}}>
-                      <View>
-                      
-                          <Icon2
-                      name="history"
-                      size={28}
-                      color="black"
-                      style={{ marginLeft:10,marginTop:10}}
-                    />
-                      
-                      </View>
-                      <View>
-                     
-                      <Text style ={{fontSize:25, fontWeight:"bold", marginLeft:10, marginTop:7}}>Appointment History</Text>
-                      </View>
-                  </View>
-                </TouchableWithoutFeedback>
-
+          <Block style={styles.apphisCard}>
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('History')}>
+            <View style={ {flexDirection:'row', marginTop:15, marginLeft:18}}>
+              <View>
+                <Icon2
+                name="history"
+                size={28}
+                color="black"
+                style={{ marginLeft:10,marginTop:10}}
+              />
                     
-                 
-                
-
-                </Block>
-
-            </ScrollView>
-          </ImageBackground>
-        </Block>
-       
-      </Block>
+              </View>
+              <View>
+                <Text style ={{fontSize:25, fontWeight:"bold", marginLeft:10, marginTop:7}}>Appointment History</Text>
+              </View>
+            </View>
+            </TouchableWithoutFeedback>
+          </Block>
+        </ScrollView>
+      </ImageBackground>
+    </Block>
+  </Block>
     );
   }
 }
@@ -352,9 +318,7 @@ const styles = StyleSheet.create({
   avatarContainer: {
     marginTop:25,
     position: "relative",
-    // marginRight:width/2
     marginLeft:20,
-    
   },
   avatar: {
     width: width * 0.26,
